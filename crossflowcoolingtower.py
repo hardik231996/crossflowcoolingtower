f018497f17ad94d1bea9d3bb0e0e1b10b7666cb4
Hardik Radadiya <hardik231996@gmail.com>
2016-04-12 13:23:10 +0530

    excel addition

--- crossflowcoolingtower.py
+++ crossflowcoolingtower.py
@@ -6,17 +6,22 @@ This is a temporary script file.
 """
 import numpy as np
 import scipy
-mw=1.0
-mg=3.0
-p=760#mm hg
-twi=35.0
-tgi=23.0
-rh=27.0
+from matplotlib import pyplot as plt
+import pandas as pd
+data=pd.read_excel("crossflowcoolingtower.xlsx","Sheet1")
+print data
+cost=scipy.array(data["Value"])
+mw=cost[0]
+mg=cost[1]
+p=cost[2]#mm hg
+twi=cost[3]#water inlet temperature
+tgi=cost[4]#air inlet temperature
+rh=cost[5]#relative humidity
 def pp(t):
-    psat=18.1016-(3644.0/(t+273.16-44.12))    
+    psat=18.1016-(3644.0/(t+273.16-44.12))    #past function
     return psat
-l=5.0
-b=3.0
+l=cost[6]
+b=cost[7]
 tw=np.zeros((101,101))
 tg=np.zeros((101,101))
 w=np.zeros((101,101))
@@ -28,10 +33,10 @@ twtz=np.zeros((101,101))
 tw=np.zeros((101,101))
 wsw=np.zeros((101,101))
 
-hd=4.1#mass transfer coefficient
-af=2.0#area per unit volume
-dx=3.0/100
-dz=5.0/100
+hd=cost[8]#mass transfer coefficient
+af=cost[9]#area per unit volume
+dx=b/100
+dz=l/100
 ga=mg/100
 for i in range(101):
     tw[i,0]=35.0#as at inlet 
@@ -58,11 +63,15 @@ for j in range(100):
 print 'inlet temperature of water',twi
 print 'inlet flowrate of water',mw  
 print 'inlet temperature of air',tgi      
-    
+twater=np.transpose(tw)  
 print 'out let temperature of water',tw[i,j+1]
 gwo=100*gw[99,99]
 print 'outlet temperature of air',tg[99,99]
 print 'water flow rate out let',gwo
+ee=plt.imshow(twater)
+plt.show()
+#as it is shown as we go down temp decreases howeveras we go from 
+#left to right cooling is less as shown in red plot"
 
      
             
